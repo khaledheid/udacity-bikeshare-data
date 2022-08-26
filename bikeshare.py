78d98dcd2148337bc41e7a67872f914fdf01e8d9
“khaledheid” <khaledheid@gmail.com>
2022-08-26 12:18:09 +0300

    Update bikeshare.py, Remove comment line 69

--- bikeshare.py
+++ bikeshare.py
@@ -66,7 +66,6 @@ def load_data(city, month, day):
     # extract month and day of week from Start Time to create new columns
     df['month'] = df["Start Time"].dt.month
     df['day_of_week'] = df["Start Time"].dt.day_name()
-    # df['day_of_week'] = df['Start Time'].dt.weekday_name
 
     # filter by both month and day if applicable
     months = ['January', 'February', 'March', 'April', 'May', 'June']
